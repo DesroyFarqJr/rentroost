c8dffbc5d577c04154244264867155c13cc06f58
Tom Fitzpatrick <tom@tomfitzpatrick.net>
2023-01-30 17:21:06 -0600

    functional mapwithpins component

--- capstone/vue/src/components/MapWithPins.vue
+++ capstone/vue/src/components/MapWithPins.vue
@@ -1,15 +1,74 @@
 <template>
-  <div>
-    SOMETHING
-  </div>
-</template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
+    <div>
+      <div>
+        <h2>adding google maps into a vue component and placing a random pin</h2>
+        <label>
+          <gmap-autocomplete @place_changed="initMarker" :options="{fields: ['geometry', 'formatted_address', 'address_components']}"></gmap-autocomplete> 
+          <!-- <gmap-autocomplete @place_changed="initMarker"></gmap-autocomplete> -->
+          <button @click="addLocationMarker">Add</button>
+        </label>
+        <br/>
+   
+      </div>
+      <br>
+      <gmap-map
+          :zoom="13"    
+          :center="center"
+          style="width:100%;  height: 600px;"
+        >
+        <gmap-marker
+          :key="index"
+          v-for="(m, index) in locationMarkers"
+          :position="m.position"
+          @click="center=m.position"
+        ></gmap-marker>
+      </gmap-map>
+    </div>
+  </template>
+   
+  <script>
+  export default {
+    name: "MapWithPins",
+    data() {
+      return {
+        center: { 
+          lat: 41.8781,
+          lng: -100.4458825
+        },
+        locationMarkers: [],
+        locPlaces: [],
+        existingPlace: null
+      };
+    },
+   
+    mounted() {
+      this.locateGeoLocation();
+    },
+   
+    methods: {
+      initMarker(loc) {
+        this.existingPlace = loc;
+      },
+      addLocationMarker() {
+        if (this.existingPlace) {
+          const marker = {
+            lat: this.existingPlace.geometry.location.lat(),
+            lng: this.existingPlace.geometry.location.lng()
+          };
+          this.locationMarkers.push({ position: marker });
+          this.locPlaces.push(this.existingPlace);
+          this.center = marker;
+          this.existingPlace = null;
+        }
+      },
+      locateGeoLocation: function() {
+        navigator.geolocation.getCurrentPosition(res => {
+          this.center = {
+            lat: res.coords.latitude,
+            lng: res.coords.longitude
+          };
+        });
+      }
+    }
+  };
+  </script>
--- capstone/vue/src/main.js
+++ capstone/vue/src/main.js
@@ -3,10 +3,18 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
+import * as VueGoogleMaps from "vue2-google-maps" // Google Maps package
 
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "", // tbd. need a way to access a variable
+    libraries: "places"
+  }
+});
 
 new Vue({
   router,
