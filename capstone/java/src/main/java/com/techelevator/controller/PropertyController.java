264e8af9680625af60615df4fe74ce543704978d
Tom Fitzpatrick <tom@tomfitzpatrick.net>
2023-02-01 17:18:47 -0600

    update property table with new columns and data

---
+++ capstone/java/src/main/java/com/techelevator/controller/PropertyController.java
@@ -0,0 +1,26 @@
+package com.techelevator.controller;
+
+import com.techelevator.model.Property;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/property")
+@PreAuthorize("isAuthenticated()")
+public class PropertyController {
+
+    public PropertyController() {
+
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.GET)
+    public List<Property> getProperties() {
+        return null;
+    }
+
+
+}
