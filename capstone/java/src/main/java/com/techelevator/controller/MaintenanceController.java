89302dfd0e4760c4a90614d9c24fd41e8a8f68cf
Desroy Farquharson <desroy78@gmail.com>
2023-02-06 21:14:30 -0500

    Maintenance Requests

---
+++ capstone/java/src/main/java/com/techelevator/controller/MaintenanceController.java
@@ -0,0 +1,31 @@
+package com.techelevator.controller;
+
+import com.techelevator.dao.MaintenanceDao;
+import com.techelevator.model.Maintenance;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+    @RestController
+    @RequestMapping("/maintenance")
+    @CrossOrigin
+    public class MaintenanceController {
+        private MaintenanceDao maintenanceDao;
+
+        public MaintenanceController(MaintenanceDao maintenanceDao) {
+            this.maintenanceDao = maintenanceDao;
+        }
+
+        @RequestMapping(path = "/", method = RequestMethod.GET)
+        public List<Maintenance> getMaintenance() {
+            return maintenanceDao.getAllMaintenanceReqs();
+        }
+
+        @RequestMapping(path ="/newticket", method = RequestMethod.POST)
+        public void createMaintenanceRequest(@RequestBody Maintenance maintenance) {
+            System.out.println(maintenance.toString());
+            maintenanceDao.createMaintenanceRequest(maintenance);
+        }
+    }
+
--- capstone/java/src/main/java/com/techelevator/dao/MaintenanceDao.java
+++ capstone/java/src/main/java/com/techelevator/dao/MaintenanceDao.java
@@ -1,12 +1,19 @@
 package com.techelevator.dao;
 
 import com.techelevator.model.Maintenance;
+import com.techelevator.model.Property;
+
+import java.util.List;
 
 public interface MaintenanceDao {
 
 
-    Maintenance getMaintenanceRequest(int maintenanceId);
+    List<Maintenance> getAllMaintenanceReqs();
+    boolean updateMaintenanceRequest(Maintenance maintenance);
 
+    void createMaintenanceRequest(Maintenance maintenance);
+    Maintenance getMaintenanceRequest(int maintenanceId);
+    List<Maintenance> searchTickets(int propertyId, String maintenanceRequest, int assignedTo, String repairStatus);
 //    MORE STUFF SOON!
 
 }
