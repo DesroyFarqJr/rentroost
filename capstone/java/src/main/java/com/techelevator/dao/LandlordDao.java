dfc8d231600c7897571214d8af0560c82870f14c
Nicholas Tsilikos <ntsilikos@gmail.com>
2023-02-09 20:34:40 -0500

    2/9 Hail mary

--- capstone/java/src/main/java/com/techelevator/dao/LandlordDao.java
+++ capstone/java/src/main/java/com/techelevator/dao/LandlordDao.java
@@ -16,11 +16,8 @@ public interface LandlordDao {
 
     boolean changeTenantAddress(String address, int id);
 
-<<<<<<< HEAD
-    ArrayList<Property> getLandlordsProperties(int landlordId);
-=======
-    ArrayList<Property> getLandlordsProperties(Principal principal);
+   ArrayList<Property> getLandlordsProperties(int landlordId);
+   // ArrayList<Property> getLandlordsProperties(Principal principal);
     Landlord getLandlordByUserId(int landlordUserId);
->>>>>>> 0f235b20790641c0e6b86467704523716da0689f
 
 }
